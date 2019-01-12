0f4a15fb84c761f4bf66412f7bd440d4f0b8b5a9
id96 <id96@cornell.edu>
2019-01-12 17:18:34 -0500

    front end modification

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import styles from '../../styles/Home.module.css';
-import Gallery from 'react-grid-gallery';
+// import styles from '../../styles/Home.module.css';
 import Intro from './Intro';
 import Footer from './Footer';
-import { IMAGES } from '../../images/volunteer_app/images.js';
+// import { IMAGES } from '../../images/volunteer_app/images.js';
 
 class Splash extends Component {
   render() {
     return (
       <div>
         <Intro />
-        <div className={styles.gallery}>
-          <Gallery enableImageSelection={false} maxRows={1} images={IMAGES} />
-        </div>
-        <Footer googleClick={this.props.onAuth} />
+        <Footer/>
       </div>
     );
   }
