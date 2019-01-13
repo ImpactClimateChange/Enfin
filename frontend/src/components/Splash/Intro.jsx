36cf03d807e9d33b6a72c65dede283f2234f86cb
id96 <id96@cornell.edu>
2019-01-13 12:10:55 -0500

    frontend mod

--- frontend/src/components/Splash/Intro.jsx
+++ frontend/src/components/Splash/Intro.jsx
@@ -5,14 +5,14 @@ class Intro extends Component {
   render() {
     return (
       <div>
-        <div className={styles.description}>
-          <div className={styles.descriptionInner}>
-            <h1>enfin: offsetting your carbon</h1>
+        <div>
+          <div className={styles.description}>
+            <div className={styles.descriptionInner}>
+              <h1><span>en</span><span>fin</span>: <span>environmental </span><span>finance</span></h1>
+              <h2>offsetting your carbon emissions</h2>
+            </div>
           </div>
         </div>
-        {/* <div className= >
-        
-        </div> */}
       </div>
     );
   }
