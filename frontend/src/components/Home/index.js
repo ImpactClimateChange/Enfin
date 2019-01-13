c50d609af3778b7f09c8360e6c0a72feafeba801
thejameswang <jwang864@gatech.edu>
2019-01-13 14:06:24 -0500

    merge and formatting

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 // import styles from '../../styles/Home.module.css';
-import Flexbox from "flexbox-react";
+import Flexbox from 'flexbox-react';
 import Progress from './Progress';
-import ImpactStatement from './ImpactStatement'
+import ImpactStatement from './ImpactStatement';
 
 class Home extends Component {
   render() {
@@ -12,14 +12,12 @@ class Home extends Component {
           <Progress />
         </div>
         <Flexbox minHeight="100vh" justifyContent="space-around">
-          <Flexbox element="header" flexGrow={2} height="60px">
+          <Flexbox element="header" height="60px">
             Emissions
           </Flexbox>
-<<<<<<< HEAD
-          <Flexbox flexGrow={1}>Impact</Flexbox>
-=======
-          <Flexbox><ImpactStatement /></Flexbox>
->>>>>>> c7de77d88a4d8989897c2df9f71ef14bc07bd16c
+          <Flexbox>
+            <ImpactStatement />
+          </Flexbox>
         </Flexbox>
       </div>
     );
