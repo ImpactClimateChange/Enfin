83ca53e6c214e6448fd83622b866999dd452afde
id96 <id96@cornell.edu>
2019-01-13 17:34:51 -0500

    footer

--- frontend/src/components/Splash/Footer.jsx
+++ frontend/src/components/Splash/Footer.jsx
@@ -8,7 +8,7 @@ class Footer extends Component {
         <div className={styles.footerBackground}>
           <div>
             <div className={styles.footer}>
-              <p>contact info here</p>
+              <p>© 2019 Enfin</p>
             </div>
           </div>
           
