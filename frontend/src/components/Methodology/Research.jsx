d40fa0d55cf8284a02b2971ce4894b83c4ff3f2b
id96 <id96@cornell.edu>
2019-01-15 17:55:34 -0500

    styling

--- frontend/src/components/Methodology/Research.jsx
+++ frontend/src/components/Methodology/Research.jsx
@@ -19,12 +19,12 @@ class Research extends Component {
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
             mollit anim id est laborum.
           </p>
-          <ul className={styles.bio}>Sources
-              <li>EPA eGRID Summary Tables and Data Files</li>
-              <li>U.S. Department of Energy</li>
-              <li>Energy Information Agency, US Census Bureau</li>
-              <li>US DOE 1605(b) Voluntary Reporting of Greenhouse Gases Program</li>
-              <li>DOE EIA Fuel Oil and Kerosene Sales 2014</li>
+          <ul className={styles.bio}><span className={styles.under}>Carbon Emission Calculation Sources</span>
+              <li>EPA eGRID <a className={styles.italic} href="http://www.epa.gov/sites/production/files/2015-10/documents/egrid2012_summarytables_0.pdf" target="_blank" rel="noopener noreferrer">Summary Tables</a> and <a className={styles.italic} href="http://www.epa.gov/energy/egrid">Data Files</a></li>
+              <li><a className={styles.italic} href="https://www.eia.gov/environment/emissions/co2_vol_mass.cfm">U.S. Department of Energy</a></li>
+              <li><a className={styles.italic} href="http://www.eia.gov/dnav/ng/ng_cons_num_a_EPG0_VN3_Count_a.htm">Energy Information Agency</a>, <a className={styles.italic} href="http://factfinder2.census.gov/faces/tableservices/jsf/pages/productview.xhtml?pid=DEC_10_DP_DPDP1&amp;prodType=table">US Census Bureau</a></li>
+              <li><a className={styles.italic} href="http://www.eia.doe.gov/oiaf/1605/coefficients.html">US DOE 1605(b) Voluntary Reporting of Greenhouse Gases Program</a></li>
+              <li><a className={styles.italic} href="http://www.eia.gov/petroleum/fueloilkerosene/pdf/foks.pdf">DOE EIA Fuel Oil and Kerosene Sales 2014</a></li>
           </ul>
         </div>
       </div>
