d871afcd58adf0cda272c054b9f13662c2546019
Abe Miller <abraham.r.m@gmail.com>
2019-02-02 15:32:03 -0800

    Add charity links

--- frontend/src/components/Home/OffsetModal.jsx
+++ frontend/src/components/Home/OffsetModal.jsx
@@ -8,26 +8,46 @@ import {Button} from 'reactstrap';
 
 const CHARITIES = [
   {
-    name: "Cool Earth", image: IMAGES.coolEarth, ratePerKg: 0.00134, reference: "", 
-    blurb: "Cool Earth is perhaps the most effective carbon offset charity. Their mission is to economically empower rural amazonian communities, relieving pressure to sell their forest land to loggers. They have a proven track-record of preventing amazonian deforestation.",
-    donateLink: "", infoLink: ""
+    name: 'Cool Earth',
+    image: IMAGES.coolEarth,
+    ratePerKg: 0.00134,
+    reference: '',
+    blurb:
+      'Cool Earth is perhaps the most effective carbon offset charity. Their mission is to economically empower rural amazonian communities, relieving pressure to sell their forest land to loggers. They have a proven track-record of preventing amazonian deforestation.',
+    donateLink: 'https://www.coolearth.org/get-involved/donate-cool-earth/',
+    infoLink: 'https://www.coolearth.org/what-we-do/our-approach/'
   },
   {
-    name: "Vegan Outreach", image: IMAGES.veganOutreach, ratePerKg: 0.00691, reference: "", 
-    blurb: "Eating a vegan diet is not only humane, it is one of the best ways to reduce carbon emissions. Vegan Outreach simultaneously reduces both emissions and animal cruelty by running the longest and most successful leafletting program for introducing people to veganism.",
-    donateLink: "", infoLink: ""
+    name: 'Vegan Outreach',
+    image: IMAGES.veganOutreach,
+    ratePerKg: 0.00691,
+    reference: '',
+    blurb:
+      'Eating a vegan diet is not only humane, it is one of the best ways to reduce carbon emissions. Vegan Outreach simultaneously reduces both emissions and animal cruelty by running the longest and most successful leafletting program for introducing people to veganism.',
+    donateLink: 'https://hub.veganoutreach.org/page/8609/donate/1?ea.tracking.id=don',
+    infoLink: 'https://veganoutreach.org/category/about/'
   },
   {
-    name: "Solar Aid", image: IMAGES.solarAid, ratePerKg: 0.0100, reference: "", 
-    blurb: "Burning kerosene is not only polluting, it is also toxic and expensive. Solar Aid provides impoverished communities in Malawi, Uganda and Zambia with reliable solar-powered lights to replace the use of kerosene lamps.",
-    donateLink: "", infoLink: ""
+    name: 'Solar Aid',
+    image: IMAGES.solarAid,
+    ratePerKg: 0.01,
+    reference: '',
+    blurb:
+      'Burning kerosene is not only polluting, it is also toxic and expensive. Solar Aid provides impoverished communities in Malawi, Uganda and Zambia with reliable solar-powered lights to replace the use of kerosene lamps.',
+    donateLink: 'https://solar-aid.org/donate/',
+    infoLink: 'https://solar-aid.org/how/'
   },
   {
-    name: "Enfin", image: images.enfin, ratePerKg: 1.00, reference: "", 
-    blurb: "Since our project is somewhat indirect, it's difficult to accurately calculate the offset efficiency. We humbly estimate to be 100x less effective than the next best option. Enfin uses your donations to maintain and improve the Enfin service. Our mission is to encourage people in the developed world to understand and act on their carbon footprint.",
-    donateLink: "", infoLink: ""
-  },
-]
+    name: 'Enfin',
+    image: images.enfin,
+    ratePerKg: 1.0,
+    reference: '',
+    blurb:
+      "Since our project is somewhat indirect, it's difficult to accurately calculate the offset efficiency. We humbly estimate to be 100x less effective than the next best option. Enfin uses your donations to maintain and improve the Enfin service. Our mission is to encourage people in the developed world to understand and act on their carbon footprint.",
+    donateLink: '',
+    infoLink: ''
+  }
+];
 
 //TODO: retrieve offset charity list from backend with request rather than having them hard coded in twice
 class OffsetModal extends Component {
