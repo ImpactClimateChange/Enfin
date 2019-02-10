934236624e1a2163da3cf8567de5b758236107e1
Abe Miller <abraham.r.m@gmail.com>
2019-02-09 16:00:21 -0800

    revert default user state to null

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: { role: 'admin' }
+      user: null
     };
   }
   componentWillMount(props) {
